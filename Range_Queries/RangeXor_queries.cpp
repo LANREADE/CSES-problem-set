d98e216b2b854fe67a64dc749dbed6d0db5d6622
LANREADE <ibrahimlanre1890@gmail.com>
2025-06-12 04:37:27 +0100

    updated

--- Range_Queries/RangeXor_queries.cpp
+++ Range_Queries/RangeXor_queries.cpp
@@ -1,50 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
+const int maxN = 2e5;
 
-#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
-#define ll long long
-#define vi vector<int>
-#define vl vector<ll>
-#define pii pair<int, int>
-#define pll pair<ll, ll>
-#define pb push_back
-#define mp make_pair
-#define fi first
-#define se second
+int N, Q, a, b;
+ll x[maxN+1];
 
-const int MOD = 1e9 + 7;
-const int INF = 1e9;
-const ll LLINF = 1e18;
-
-
-void solve() {
-        int n, q;
-        cin >> n >> q;
-
-        vl prefix_Sum( n + 1 );
-        prefix_Sum[0] = 0;
-        for(int i = 1 ; i <= n ; i++){
-                int x;
-                cin >> x;
-                prefix_Sum[i] = prefix_Sum[i - 1] + x;
-        }
-        for(int i = 0 ; i < q ; i++){
-                int a , b;
-                cin >> a >> b;
-
-                ll result = prefix_Sum[b] - prefix_Sum[ a - 1];
-                cout << result << "\n";
-        }
-}
-
-int main() {
-    fastio;
-
-    int tc = 1; // Number of test cases
-    // cin >> tc;
-    while (tc--) {
-        solve();
+int main(){
+    scanf("%d %d", &N, &Q);
+    for(int i = 1; i <= N; i++){
+        scanf("%lld", &x[i]);
+        x[i] ^= x[i-1]; // just using the idea of the xor operator;
     }
-
-    return 0;
+    for(int q = 0; q < Q; q++){
+        scanf("%d %d", &a, &b);
+        printf("%lld\n", x[b]^x[a-1]);
+    }
 }
